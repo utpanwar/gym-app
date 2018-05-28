0dcb3540c11c203e6b89262b0a83b2241e527b13
utpanwar <39692831+utpanwar@users.noreply.github.com>
2018-05-28 15:42:49 +0530

    Add files via upload

---
+++ workout app/js/7minworkout/workout.js
@@ -0,0 +1,32 @@
+angular.module('7minWorkout').controller('WorkoutController',
+ function($scope){
+
+   function Exercise(args) {
+ this.name = args.name;
+ this.title = args.title;
+ this.description = args.description;
+ this.image = args.image;
+ this.related = {};
+ this.related.videos = args.videos;
+ this.nameSound = args.nameSound;
+ this.procedure=args.procedure;
+}
+
+function WorkoutPlan(args) {
+ this.exercises = [];
+ this.name = args.name;
+ this.title = args.title;
+ this.restBetweenExercise = args.restBetweenExercise;
+};
+
+var restExercise;
+var workoutPlan;
+
+
+var init = function () {
+startWorkout();
+};
+init();
+
+
+});
